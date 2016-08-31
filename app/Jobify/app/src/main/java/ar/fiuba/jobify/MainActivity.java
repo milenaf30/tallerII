f08d62df29ab54d7a04b997c6a8a60e7a98e504f
martincura <martincura@gmail.com>
2016-08-31 02:35:09 -0300

    App puede GETtear una URL para obtener información. Funcionalidad de prueba nomás.

--- app/Jobify/app/src/main/java/ar/fiuba/jobify/MainActivity.java
+++ app/Jobify/app/src/main/java/ar/fiuba/jobify/MainActivity.java
@@ -2,12 +2,27 @@ package ar.fiuba.jobify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.net.Uri;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements PerfilFragment.OnFragmentInteractionListener {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PerfilFragment())
+                    .commit();
+        }
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
