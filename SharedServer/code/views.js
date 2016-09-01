79ba9fdf2b3f37f97e34e357e034eff921f4c6c9
Martín Volpe <martin.volpe@gmail.com>
2016-09-01 12:46:14 -0300

    Primer archivo de travis cargado para probar el build.

--- SharedServer/code/views.js
+++ SharedServer/code/views.js
@@ -10,33 +10,18 @@ module.exports.job_positions_add = function (req, res) {
   description = "test description";
   category = "test category";
 
+  //TODO // text formatting
   db.pool.query('INSERT INTO job_positions(name, description, category) ' + 
     "values('" + name + "','" + description + "','" + category + "')"
     ).then(function () {
       res.status(200)
         .json({
           status: 'success',
-          message: 'Inserted one puppy'
+          message: 'Se agrego el trabajo'
         });
     })
     .catch(function (err) {
       return next(err);
     });
 
-  /*req.body.age = parseInt(req.body.age);
-  db.none('insert into pups(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
-    req.body)
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-    
-    res.json("HELLO WORD! + {req.params.tagId}");*/
-};
+}
--- SharedServer/tests/run_test.sh
+++ SharedServer/tests/run_test.sh
@@ -1,2 +1,2 @@
-docker build -t sharedservertest . 
+docker build -t sharedservertest $1/. 
 docker run -it sharedservertest bash
