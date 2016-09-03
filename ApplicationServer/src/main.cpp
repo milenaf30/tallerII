98e4fbd84505fa879fa4bad242a84630ef0b5913
Milena <milenafarotto@gmail.com>
2016-09-03 20:21:44 -0300

    funcando de la manera más simple

--- ApplicationServer/src/main.cpp
+++ ApplicationServer/src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "ApplicationServer_config.h"
+
 
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "Version " << ApplicationServer_VERSION_MAJOR << "." << ApplicationServer_VERSION_MINOR << endl;
+    //cout << "Version " << ApplicationServer_VERSION_MAJOR << "." << ApplicationServer_VERSION_MINOR << endl;
     return 0;
 }
